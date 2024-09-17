d6258fe47018c6e9c168db3c65549e9d743cf4a5
ITR <ITR@live.no>
2024-09-17 23:18:03 +0200

    Implement asking per profession.
    Allows asking somebody what they think a specific profession would answer a question with.
    Doesn't allow your root question to be to a profession because you don't know who has that profession.

--- strats/game/consts.py
+++ strats/game/consts.py
@@ -6,6 +6,11 @@ Bob     = types.Person("Bob",     1)
 Charlie = types.Person("Charlie", 2)
 Dan     = types.Person("Dan",     3)
 
+Mathematician = types.Person("Mathematician", 4)
+Physicist     = types.Person("Physicist",     5)
+Engineer      = types.Person("Engineer",      6)
+Philosopher   = types.Person("Philosopher",   7)
+
 # responses
 Foo = types.Response("foo")
 Bar = types.Response("bar")
