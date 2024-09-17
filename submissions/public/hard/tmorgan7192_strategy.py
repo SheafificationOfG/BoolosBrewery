da7e2f862c0f986175ce625853c3c7eced15b1de
Thomas Morgan <tmorgan@Thomass-MacBook-Pro.local>
2024-09-17 11:28:02 -0500

    Add submission

---
+++ submissions/public/hard/tmorgan7192_strategy.py
@@ -0,0 +1,131 @@
+from strats import *
+
+class Strategy(Hard):
+
+    """
+
+    Your strategy *must* be implemented in a class called "Strategy".
+
+    The target puzzle variant is specified by deriving from "Easy" (as shown above), "Default", or "Hard".
+
+    """
+
+ 
+
+    question_limit = 7
+
+    """
+
+    Your strategy *must* specify an upper bound for how many questions it will ask.
+
+    You will automatically fail if you ask more questions than the limit you specify.
+
+    However, you may ask fewer questions than your allocated limit.
+
+    Our strategy uses exactly four questinos in all cases.
+
+    """
+
+ 
+
+    def solve(game):
+
+        """
+
+        We will implement our strategy logic in this function!
+
+        """
+        print()
+
+        people = [Alice, Bob, Charlie, Dan]
+        subjects = [Math, Engg, Phys, Phil]
+        responses = []
+        guesses = []
+
+        for person in people:
+            responses.append(game.get_response(person.ask(True)))
+
+        found_match = False
+        for i in range(4):
+            for j in range(4):
+              if i == j:
+                continue
+
+              if responses[i] == responses[j]:
+                maybe_engg_indexes = [i,j]
+                not_engg_indexes = list(filter(lambda x: x not in maybe_engg_indexes, range(4)))
+
+                found_match = True
+                break
+
+            if found_match:
+              break
+
+        question_5_response = game.get_response(people[not_engg_indexes[0]].ask(False))
+
+        if question_5_response == responses[not_engg_indexes[0]]:
+            game.guess[people[not_engg_indexes[0]]] = Phil
+            guesses.append((not_engg_indexes[0], Phil))
+
+            maybe = responses[not_engg_indexes[0]]
+            yes_or_no = list(filter(lambda x: x != maybe, [Foo, Bar, Baz]))
+
+            math_or_phys_person_index = not_engg_indexes[1]
+            other_person_index = not_engg_indexes[0]
+
+        else:
+            math_or_phys_person_index = not_engg_indexes[0]
+            other_person_index = not_engg_indexes[1]
+
+            yes_or_no = [responses[not_engg_indexes[0]], question_5_response]
+            maybe = list(filter(lambda x: x not in yes_or_no, [Foo, Bar, Baz]))[0]
+
+            if maybe == responses[not_engg_indexes[1]]:
+                game.guess[people[not_engg_indexes[1]]] = Phil
+                guesses.append((not_engg_indexes[1], Phil))
+
+        print("yes_or_no: [" + yes_or_no[0]._name + ", " + yes_or_no[1]._name + "], maybe: " + maybe._name)
+        
+        math_or_phys_person = people[math_or_phys_person_index]
+        math_or_phys_response = responses[math_or_phys_person_index]
+
+        question_6_response = game.get_response(math_or_phys_person.ask(math_or_phys_person.ask(math_or_phys_person.studies(Math)).equals(math_or_phys_response)))
+
+        if question_6_response == math_or_phys_response:
+            game.guess[math_or_phys_person] = Math
+            other_subject = Phys
+            guesses.append((math_or_phys_person_index, Math))
+
+        else:
+            game.guess[math_or_phys_person] = Phys
+            other_subject = Math
+            guesses.append((math_or_phys_person_index, Phys))
+
+        if len(guesses) == 1:
+            game.guess[people[other_person_index]] = other_subject
+            guesses.append((other_person_index, other_subject))
+
+        question_7_response = game.get_response(math_or_phys_person.ask(math_or_phys_person.ask(people[maybe_engg_indexes[0]].studies(Engg)).equals(math_or_phys_response)))
+
+        if question_7_response == math_or_phys_response:
+
+            engg_index = maybe_engg_indexes[0]
+
+        else:
+
+            engg_index = maybe_engg_indexes[1]
+
+        game.guess[people[engg_index]] = Engg
+        guesses.append((engg_index, Engg))
+
+        missing_index = list(filter(lambda x: x not in map(lambda y: y[0], guesses), range(4)))[0]
+
+        missing_subject = list(filter(lambda x: x not in map(lambda y: y[1], guesses), subjects))[0]
+
+        game.guess[people[missing_index]] == missing_subject
+        guesses.append((missing_index, missing_subject))
+
+        for (person_index, subject) in guesses:
+            print("My guess: " + people[person_index]._name + ", " + subject._name)
+
+        print()
