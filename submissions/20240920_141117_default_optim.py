b5bd992841f2570b0f69da63470887c7fce2af84
Oliver Weissl <Oliver Weissl>
2024-09-21 10:46:22 +0200

    further optimized

---
+++ submissions/20240920_141117_default_optim.py
@@ -0,0 +1,16 @@
+from strats import *
+
+
+class Strategy(Default):
+    # This solution is based on vzsky`s approach, im simply trying to lower complexity score.
+    question_limit = 3
+
+
+    def solve(self):
+        ask = lambda x, y, z: self.get_response(x.ask(x.ask(y.studies(z)).equals(Foo))) == Foo
+
+        b, c = (b, Charlie) if ask((a:=Alice), (b:=Bob), Engg) else (Charlie, b)
+
+        if ask(c, b, Engg): a, b = b, a
+
+        self.guess[a], self.guess[b], self.guess[c] = [Engg, Phys, Math] if ask(c, c, Math) else [Engg, Math, Phys]
