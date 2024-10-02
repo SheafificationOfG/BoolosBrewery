6f777aec9ccda86c732c75feedd84aeba49cebb7
ITR <ITR@live.no>
2024-10-02 05:16:35 +0200

    Added low complexity Normal solution

---
+++ submissions/itr.py
@@ -0,0 +1,16 @@
+from strats import *
+
+class Strategy(Default):
+    question_limit = 3
+
+    def solve(self):
+        answers = ["Bob","Charlie","Alice"]
+        fields = (Engg,Phys,Math)
+        def ask(other):
+            answers.append(answers.pop(1 if self.get_response(Question(answers[2], f'{answers[other]} studies {fields[other//2]} iff Foo'))==Foo else other))
+
+        ask(0)
+        ask(0)
+        ask(2)
+
+        self._guess = dict(zip(answers, fields))
